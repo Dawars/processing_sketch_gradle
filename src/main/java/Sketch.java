c97824b91cdb8f58e99b7abd8efeca8836bfb970
Dawars <dawars00@gmail.com>
2017-12-30 00:05:22 +0100

    Initial commit

---
+++ src/main/java/Sketch.java
@@ -0,0 +1,14 @@
+import processing.core.PApplet;
+
+public class Sketch extends PApplet {
+
+    @Override
+    public void settings() {
+        size(200, 200, P3D);
+    }
+
+    public static void main(String[] args) {
+        PApplet.main(Sketch.class);
+    }
+
+}
